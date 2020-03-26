e5947aae95af5270c5ec707aa614462441e8bf4b
Thomas Cohn <cohnt@umich.edu>
2020-03-26 13:14:30 -0400

    Embed the data and plot the embedding.

--- swiss_roll.py
+++ swiss_roll.py
@@ -3,7 +3,14 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn import manifold, datasets
 
-n_points = 500
+n_points = 600
 X, color = datasets.make_swiss_roll(n_points)
-n_neighbors = 12
+n_neighbors = 6
 n_components = 2
+
+method = manifold.Isomap(n_neighbors, n_components)
+Y = method.fit_transform(X)
+
+fig, ax = plt.subplots()
+ax.scatter(Y[:,0], Y[:,1], c=color, cmap=plt.cm.Spectral)
+plt.show()
