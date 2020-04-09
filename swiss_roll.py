752d3bee64d007af96ce252bd9c2c19ebec3aeb4
Thomas Cohn <cohnt@umich.edu>
2020-04-09 14:40:50 -0400

    Plot both dimensions.

--- swiss_roll.py
+++ swiss_roll.py
@@ -32,11 +32,12 @@ f_2 = d.fill_rips(Y, k_skeleton, max_epsilon)
 p_2 = d.homology_persistence(f_2)
 dgms_2 = d.init_diagrams(p_2, f_2)
 
-which_diagram = 1
+for which_diagram in [0, 1]:
+	fig, axes = plt.subplots(nrows=2, ncols=2)
+	d.plot.plot_diagram(dgms_1[which_diagram], ax=axes[0,0], show=False)
+	d.plot.plot_diagram(dgms_2[which_diagram], ax=axes[0,1], show=False)
+	d.plot.plot_bars(dgms_1[which_diagram], ax=axes[1,0], show=False)
+	d.plot.plot_bars(dgms_2[which_diagram], ax=axes[1,1], show=False)
+	fig.suptitle("Dimension %d" % which_diagram)
 
-fig, axes = plt.subplots(nrows=2, ncols=2)
-d.plot.plot_diagram(dgms_1[which_diagram], ax=axes[0,0], show=False)
-d.plot.plot_diagram(dgms_2[which_diagram], ax=axes[0,1], show=False)
-d.plot.plot_bars(dgms_1[which_diagram], ax=axes[1,0], show=False)
-d.plot.plot_bars(dgms_2[which_diagram], ax=axes[1,1], show=False)
 plt.show()
